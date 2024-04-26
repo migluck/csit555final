d6507c0d6fff4272a91b3b018099a057b2c5756d
Decabra <sarmadcute83@gmail.com>
2024-04-26 17:02:43 -0400

    CRUD Endpoints

--- database/schema.sql
+++ database/schema.sql
@@ -5,9 +5,9 @@ CREATE TABLE `payment` (
        `id`           INT AUTO_INCREMENT PRIMARY KEY,
        `company`      VARCHAR(255),
        `amount`       FLOAT,
-       `payment_date` DATE,
+       `payment_date` VARCHAR(255),
        `status`       VARCHAR(255),
-       `due_date`     DATE,
+       `due_date`     VARCHAR(255),
        `created_at`   TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
        `updated_at`   TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
--- payment_record/src/controllers/Payment.py
+++ payment_record/src/controllers/Payment.py
@@ -1,25 +1,54 @@
 from flask import Blueprint, request, jsonify, render_template
-from src.services.payments import get_all_payment, create_payment
+from src.services  import payments
 import logging
 
-bp = Blueprint('payment', __name__, url_prefix='/payment')
+bp = Blueprint('payment', __name__, url_prefix='/payments')
 
-
-@bp.route('/all', methods=['GET'])
+# Get all payments
+@bp.route('/', methods=['GET'])
 def index():
     logging.info("Fetching all payments")
     try:
-        data = get_all_payment()
-        logging.info(f"Data fetched dsadsa successfully, {data}")
+        data = payments.get_all_payments()
+        logging.info(f"Data fetched successfully, {data}")
         return jsonify(data), 200
     except Exception as e:
         logging.error(f"Error fetching payments: {e}")
         return jsonify({"error": str(e)}), 500
 
+# Create new payment
 @bp.route('/', methods=['POST'])
-def add_payment():
+def create_payment():
     data = request.get_json()
-    payment = create_payment(data)
-    return jsonify(payment.to_dict()), 201
+    try:
+        new_payment = payments.create_payment(data)
+        return jsonify(new_payment.to_dict()), 201
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
 
+# Get payment by id
+@bp.route('/<int:id>', methods=['GET'])
+def get_payment(id):
+    payment = payments.get_payment_by_id(id)
+    if payment:
+        return jsonify(payment.to_dict())
+    return jsonify({"error": "Payment not found"}), 404
+
+# Update payment by id
+@bp.route('/<int:id>', methods=['PUT'])
+def update_payment(id):
+    data = request.get_json()
+    payment = payments.update_payment(id, data)
+    if payment:
+        return jsonify(payment.to_dict())
+    else:
+        return jsonify({"error": "Payment not found"}), 404
+
+# Delete payment by id
+@bp.route('/<int:id>', methods=['DELETE'])
+def delete_payment(id):
+    success = payments.delete_payment(id)
+    if success:
+        return jsonify({"success": "Payment deleted"}), 200
+    return jsonify({"error": "Payment not found"}), 404
 
