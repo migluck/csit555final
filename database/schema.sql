59bf7e51309b0217a9fb6ee7ab5431b6d5af6753
Decabra <sarmadcute83@gmail.com>
2024-04-27 21:29:09 -0400

    Minor improvements

--- database/schema.sql
+++ database/schema.sql
@@ -1,13 +1,19 @@
-CREATE DATABASE IF NOT EXISTS tax_payment_system;
-USE tax_payment_system;
+CREATE
+DATABASE IF NOT EXISTS tax_payment_system;
+USE
+tax_payment_system;
 -- Payment Table
-CREATE TABLE `payment` (
-       `id`           INT AUTO_INCREMENT PRIMARY KEY,
-       `company`      VARCHAR(255),
-       `amount`       FLOAT,
-       `payment_date` VARCHAR(255),
-       `status`       VARCHAR(255),
-       `due_date`     VARCHAR(255),
-       `created_at`   TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-       `updated_at`   TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+CREATE TABLE `payment`
+(
+    `id`           INT AUTO_INCREMENT PRIMARY KEY,
+    `company`      VARCHAR(255),
+    `amount`       FLOAT,
+    `payment_date` VARCHAR(255),
+    `status`       VARCHAR(255),
+    `due_date`     VARCHAR(255),
+    `created_at`   TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `updated_at`   TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    INDEX          `idx_company` (`company`),           -- Index on company
+    INDEX          `idx_payment_date` (`payment_date`), -- Index on payment date
+    INDEX          `idx_due_date` (`due_date`),         -- Index on due date
 );
